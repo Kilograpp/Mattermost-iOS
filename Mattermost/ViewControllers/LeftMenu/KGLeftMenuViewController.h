bcf010b6da03b432284339e99fc61c37d6af4cbf
Igor Vedeneev <getmaxx@hotmail.com>
2016-06-09 18:37:05 +0300

    Добавлено левое меню имплементация хедера

---
+++ Mattermost/ViewControllers/LeftMenu/KGLeftMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  KGLeftMenuViewController.h
+//  Mattermost
+//
+//  Created by Igor Vedeneev on 09.06.16.
+//  Copyright © 2016 Kilograpp. All rights reserved.
+//
+
+#import "KGViewController.h"
+
+@interface KGLeftMenuViewController : KGViewController
+
+@end
