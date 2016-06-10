fc4a2190efd0b4369f5799c33352a6fbf68429e7
Maxim Gubin <maxim@kilograpp.com>
2016-06-10 19:13:43 +0300

    Добавлена интеграция со свифтом

---
+++ Mattermost/Supporting Files/Mattermost-Swift.h
@@ -0,0 +1,13 @@
+//
+//  Mattermost-Swift.h
+//  Mattermost
+//
+//  Created by Maxim Gubin on 10/06/16.
+//  Copyright © 2016 Kilograpp. All rights reserved.
+//
+
+#ifndef Mattermost_Swift_h
+#define Mattermost_Swift_h
+
+
+#endif /* Mattermost_Swift_h */
