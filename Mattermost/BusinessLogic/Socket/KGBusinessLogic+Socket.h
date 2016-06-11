4da1f9821204a29a76832fa80262e335656a14c9
Maxim Gubin <maxim@kilograpp.com>
2016-06-11 16:23:37 +0300

    Добавлен фундамент сокетов

---
+++ Mattermost/BusinessLogic/Socket/KGBusinessLogic+Socket.h
@@ -0,0 +1,15 @@
+//
+// Created by Maxim Gubin on 11/06/16.
+// Copyright (c) 2016 Kilograpp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "KGBusinessLogic.h"
+
+@class SRWebSocket;
+
+@interface KGBusinessLogic (Socket)
+
+- (void)rocketFuckingSocket;
+
+@end
