8f2f34fb2077705997c2f660bce6edb440ad5ced
Tatiana <skorbilinatatiana@kilograpp.com>
2016-06-21 19:33:47 +0300

    Добавлена возможность изменения аватарки

--- Mattermost/ViewControllers/Profile/KGProfileTableViewController.m
+++ Mattermost/ViewControllers/Profile/KGProfileTableViewController.m
@@ -13,14 +13,22 @@
 #import "UIImageView+UIActivityIndicatorForSDWebImage.h"
 #import "UIImage+Resize.h"
 #import "KGBusinessLogic+Session.h"
+#import "KGAlertManager.h"
+#import "UIImage+KGRotate.h"
+#import "KGImagePickerController.h"
 
-@interface KGProfileTableViewController ()
+@import AVFoundation;
+@import Photos;
+
+
+@interface KGProfileTableViewController () <UIImagePickerControllerDelegate>
 //FIXME: код стайл и названия
 @property (weak, nonatomic) IBOutlet UIImageView *avatarImageView;
-@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *nameTitleLabel;
 @property (weak, nonatomic) IBOutlet UILabel *name;
 @property (weak, nonatomic) IBOutlet UILabel *username;
-
+@property (weak, nonatomic) IBOutlet UIView *headerView;
+@property (nonatomic, strong) UIImage *updatedAvatarImage;
 @property (weak, nonatomic) IBOutlet UILabel *nickname;
 @property (weak, nonatomic) IBOutlet UILabel *email;
 
@@ -57,19 +65,99 @@
 }
 
 - (void)setup {
-    KGUser *user = [KGUser managedObjectById:self.userId];
-    self.nameLabel.font = [UIFont kg_semibold30Font];
-    self.nameLabel.textColor = [UIColor kg_blackColor];
+//    KGUser *user = [KGUser managedObjectById:self.userId];
+    KGUser *user = [[KGBusinessLogic sharedInstance]currentUser];
+    self.nameTitleLabel.font = [UIFont kg_semibold30Font];
+    self.nameTitleLabel.textColor = [UIColor kg_blackColor];
     self.avatarImageView.layer.cornerRadius = CGRectGetHeight(self.avatarImageView.bounds) / 2;
     self.avatarImageView.clipsToBounds = YES;
     self.avatarImageView.backgroundColor = [UIColor whiteColor];
-    self.nameLabel.text = user.nickname;
+    self.nameTitleLabel.text = user.nickname;
     [self.avatarImageView setImageWithURL:user.imageUrl placeholderImage:nil options:SDWebImageHandleCookies
               usingActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
     self.name.text = user.firstName;
     self.username.text = user.nickname;
     self.nickname.text = user.nickname;
     self.email.text = user.email;
+    //self.headerView.backgroundColor = [UIColor kg_lightLightGrayColor];
 }
+
+
+- (CGFloat) tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    if (section == 0){
+        return CGFLOAT_MIN;
+    } else {
+        return 30.f;
+    }
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:nil message:nil preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    UIAlertAction *openCameraAction =
+    [UIAlertAction actionWithTitle:NSLocalizedString(@"Сделать фото", nil) style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
+        
+        switch ([AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo]) {
+            case AVAuthorizationStatusRestricted:
+            case AVAuthorizationStatusDenied:
+                [[KGAlertManager sharedManager] showErrorWithTitle:@"Нет доступа к камере"
+                                                           message:@"Пожалуйста разрешите использовать камеру в настройках"];
+                break;
+                
+            default:
+                [self presentPickerControllerWithType:UIImagePickerControllerSourceTypeCamera];
+                break;
+        }
+    }];
+    
+    UIAlertAction *openGalleryAction =
+    [UIAlertAction actionWithTitle:NSLocalizedString(@"Выбрать из библиотеки", nil) style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
+        switch ([PHPhotoLibrary authorizationStatus]) {
+            case AVAuthorizationStatusRestricted:
+            case AVAuthorizationStatusDenied:
+                [[KGAlertManager sharedManager] showErrorWithTitle:@"Нет доступа к фотографиям"
+                                                           message:@"Пожалуйста разрешите использовать фотографии в настройках"];
+                break;
+                
+            default: {
+                [self presentPickerControllerWithType:UIImagePickerControllerSourceTypePhotoLibrary];
+                break;
+            }
+        }
+    }];
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:NSLocalizedString(@"Отмена", nil) style:UIAlertActionStyleCancel handler:nil];
+    [alertController addAction:openCameraAction];
+    [alertController addAction:openGalleryAction];
+    [alertController addAction:cancelAction];
+    
+    [self presentViewController:alertController animated:YES completion:nil];
+
+}
+
+- (void)presentPickerControllerWithType:(UIImagePickerControllerSourceType)type {
+    KGImagePickerController *pickerController = [[KGImagePickerController alloc] init];
+    pickerController.sourceType = type;
+    pickerController.delegate = self;
+    
+    [self presentViewController:pickerController animated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    UIImage *image = [info objectForKey:@"UIImagePickerControllerOriginalImage"];
+    if (picker.sourceType == UIImagePickerControllerSourceTypeCamera) {
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil);
+    }
+    
+    self.updatedAvatarImage = [image kg_normalizedImage];
+    if (self.updatedAvatarImage){
+        self.avatarImageView.image = self.updatedAvatarImage;
+        [[KGBusinessLogic sharedInstance] updateImageForCurrentUser:self.updatedAvatarImage withCompletion:nil];
+        [[SDImageCache sharedImageCache] storeImage:self.updatedAvatarImage forKey:[[KGBusinessLogic sharedInstance] currentUser].imageUrl.absoluteString];
+ 
+    }
+    //[self.tableHeaderView reloadWithImage:self.updatedAvatarImage];
+}
+
 
 @end
