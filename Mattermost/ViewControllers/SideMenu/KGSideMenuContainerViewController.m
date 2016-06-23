84a84df2b9f885e7d3b480ee2cb32d4e31da26e6
Maxim Gubin <maxim@kilograpp.com>
2016-06-23 23:14:19 +0300

    Добавлен скользящий статус бар

--- Mattermost/ViewControllers/SideMenu/KGSideMenuContainerViewController.m
+++ Mattermost/ViewControllers/SideMenu/KGSideMenuContainerViewController.m
@@ -11,7 +11,16 @@
 #import "KGConstants.h"
 #import "KGLeftMenuViewController.h"
 #import "KGRightMenuViewController.h"
+#import "UIStatusBar+SharedBar.h"
 #import <Masonry/Masonry.h>
+
+
+// Public Morozov
+@interface MFSideMenuContainerViewController()
+
+- (void) setCenterViewControllerOffset:(CGFloat)xOffset;
+
+@end
 
 @interface KGSideMenuContainerViewController ()
 @property (nonatomic, assign) CGFloat *oldX;
@@ -73,6 +82,14 @@
   }
 
 
+- (void) setCenterViewControllerOffset:(CGFloat)xOffset {
+    UIView *bar = [UIStatusBar sharedStatusBar];
+    CGRect frame = bar.frame;
+    frame.origin.x = xOffset;
+    [bar setFrame:frame];
+
+    [super setCenterViewControllerOffset:xOffset];
+}
 #pragma mark - Orientations
 
 - (UIInterfaceOrientationMask)supportedInterfaceOrientations {
