a3e7a184b87686ee800f9b13e2b7f8f66ff0f3b8
Tatiana <skorbilinatatiana@kilograpp.com>
2016-07-20 11:30:50 +0300

    Refactor

--- Mattermost/NavigationControllers/Chat/KGChatNavigationController.h
+++ Mattermost/NavigationControllers/Chat/KGChatNavigationController.h
@@ -10,15 +10,12 @@
 @class KGChannel;
 
 @protocol KGChatNavigationDelegate <NSObject>
-//FIXME: метод совершает действие - название должно содержать действие: например, navigateToChannelMembersList и чуть больше информативности.
-//FIXME: делагат должен выполнять дейтсвие по нажатию, т.е это не обязательно переход к членам канала. название метода должно это отображать - например didSelectTitleViewWithChannelIdentifier:(NSString *)channelId
-- (void)navigationToMembers;
+- (void)didSelectTitleView;
 @end
 
 @interface KGChatNavigationController : KGNavigationController
 
 - (void)configureTitleViewWithChannel:(KGChannel *)channel
                  loadingInProgress:(BOOL)loadingInProgress;
-//FIXME: у UINavigationController уже есть делегат, стоит переименовать переменную в kg_delegate или аналогичное
-@property (nonatomic, weak) id<KGChatNavigationDelegate> delegate;
+@property (nonatomic, weak) id<KGChatNavigationDelegate> kg_delegate;
 @end
--- Mattermost/NavigationControllers/Chat/KGChatNavigationController.m
+++ Mattermost/NavigationControllers/Chat/KGChatNavigationController.m
@@ -51,12 +51,17 @@
     self.navigationBar.barTintColor = [UIColor kg_navigationBarTintColor];
     
     [self setupTitleLabel];
-    
-    //FIXME: вынести в отдельные методы конфигурацию titleView и UITapGestureRecognizer
+    [self setupTitleView];
+    [self setupGestureRecognizer];
+}
+
+- (void)setupTitleView {
     self.kg_titleView = [[KGNavigationBarTitleView alloc] init];
     self.kg_titleView.frame = CGRectMake(0.f, 0.f, KGScreenWidth() * 0.6f, 44.f);
     [self.navigationBar.topItem setTitleView:self.kg_titleView];
-    
+}
+
+- (void)setupGestureRecognizer {
     [self.kg_titleView.titleLabel setUserInteractionEnabled:YES];
     [self.kg_titleView.titleLabel addGestureRecognizer:[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(showMembers)]];
 }
@@ -67,7 +72,7 @@
 }
 
 - (void)showMembers {
-    [self.delegate navigationToMembers];
+    [self.kg_delegate didSelectTitleView];
 }
 
 #pragma mark - Status Bar
